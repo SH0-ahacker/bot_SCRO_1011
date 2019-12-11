47971fdaa2f78670429b7c296dbb80eb1129b2fe
lagalapizza_1011 <52937131+SH0-ahacker@users.noreply.github.com>
2019-12-10 21:20:54 -0300

    Create form.js

---
+++ V 4.2.8/body/form.js
@@ -0,0 +1,4 @@
+let name = prompt("what is your name (1/3)")
+let age = prompt("what is your age? (2/3)")
+let bestfriends = prompt("what is your bestfriends? (3/3)")
+ alert("okay!, " + name + "!!!!!")
